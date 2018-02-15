854a5865cb46b467f4a0e41a52e8ad617904b28e
Jeff Cohen <cohen.jeff@gmail.com>
2018-02-15 12:16:33 -0600

    Ready

---
+++ app/controllers/conditions_controller.rb
@@ -0,0 +1,7 @@
+class ConditionsController < ApplicationController
+
+  def index
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :conditions
+
+  root 'conditions#index'
+
 end
